2116ea8f6b9c059a00db1411647b3c6ddbf39ec1
Akihiro Nagami <nagami@kikyos.co.jp>
2019-04-07 16:58:16 +0900

    完成。力技すぎて笑う

--- FiveRatings/ViewController.swift
+++ FiveRatings/ViewController.swift
@@ -22,6 +22,52 @@ class ViewController: UIViewController {
         fifthButton.setImage(UIImage.init(named: "colorless_star_icon.png"), for: UIControl.State.normal)
         
     }
+    
+    @IBAction func firstButton(_ sender: Any) {
+        firstButton.setImage(UIImage.init(named: "yellow_star_icon.png"), for: UIControl.State.normal)
+        secondButton.setImage(UIImage.init(named: "colorless_star_icon.png"), for: UIControl.State.normal)
+        thirdButton.setImage(UIImage.init(named: "colorless_star_icon.png"), for: UIControl.State.normal)
+        fourthButton.setImage(UIImage.init(named: "colorless_star_icon.png"), for: UIControl.State.normal)
+        fifthButton.setImage(UIImage.init(named: "colorless_star_icon.png"), for: UIControl.State.normal)
+    }
+    
+    @IBAction func secondButton(_ sender: Any) {
+        firstButton.setImage(UIImage.init(named: "yellow_star_icon.png"), for: UIControl.State.normal)
+        secondButton.setImage(UIImage.init(named: "yellow_star_icon.png"), for: UIControl.State.normal)
+        thirdButton.setImage(UIImage.init(named: "colorless_star_icon.png"), for: UIControl.State.normal)
+        fourthButton.setImage(UIImage.init(named: "colorless_star_icon.png"), for: UIControl.State.normal)
+        fifthButton.setImage(UIImage.init(named: "colorless_star_icon.png"), for: UIControl.State.normal)
+    }
+    
+    @IBAction func thirdButton(_ sender: Any) {
+        firstButton.setImage(UIImage.init(named: "yellow_star_icon.png"), for: UIControl.State.normal)
+        secondButton.setImage(UIImage.init(named: "yellow_star_icon.png"), for: UIControl.State.normal)
+        thirdButton.setImage(UIImage.init(named: "yellow_star_icon.png"), for: UIControl.State.normal)
+        fourthButton.setImage(UIImage.init(named: "colorless_star_icon.png"), for: UIControl.State.normal)
+        fifthButton.setImage(UIImage.init(named: "colorless_star_icon.png"), for: UIControl.State.normal)
+    }
+    
+    
+    @IBAction func fourthButton(_ sender: Any) {
+        firstButton.setImage(UIImage.init(named: "yellow_star_icon.png"), for: UIControl.State.normal)
+        secondButton.setImage(UIImage.init(named: "yellow_star_icon.png"), for: UIControl.State.normal)
+        thirdButton.setImage(UIImage.init(named: "yellow_star_icon.png"), for: UIControl.State.normal)
+        fourthButton.setImage(UIImage.init(named: "yellow_star_icon.png"), for: UIControl.State.normal)
+        fifthButton.setImage(UIImage.init(named: "colorless_star_icon.png"), for: UIControl.State.normal)
+    }
+    
+    @IBAction func fifthButton(_ sender: Any) {
+        firstButton.setImage(UIImage.init(named: "yellow_star_icon.png"), for: UIControl.State.normal)
+        secondButton.setImage(UIImage.init(named: "yellow_star_icon.png"), for: UIControl.State.normal)
+        thirdButton.setImage(UIImage.init(named: "yellow_star_icon.png"), for: UIControl.State.normal)
+        fourthButton.setImage(UIImage.init(named: "yellow_star_icon.png"), for: UIControl.State.normal)
+        fifthButton.setImage(UIImage.init(named: "yellow_star_icon.png"), for: UIControl.State.normal)
+    }
+    
+    
+    
+    
+    
 
 
 }
